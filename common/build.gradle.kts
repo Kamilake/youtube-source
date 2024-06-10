ff4c4a3322862c530b7a9fd41eb36dd4d6060ab0
Kamilake <exjang0@gmail.com>
2024-06-10 10:54:36 +0900

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,9 +1,15 @@
 import com.vanniktech.maven.publish.JavaLibrary
 import com.vanniktech.maven.publish.JavadocJar
+import org.gradle.api.JavaVersion
 
 plugins {
     `java-library`
     alias(libs.plugins.maven.publish.base)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 base {
