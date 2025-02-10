e947775aa1733677cf05f5e6155ff43dc3bf3653
Kamilake <exjang0@gmail.com>
2025-02-10 14:27:16 +0900

    .

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,7 @@ dependencyResolutionManagement {
       library("slf4j", "org.slf4j", "slf4j-api").version("1.7.25")
       library("annotations", "org.jetbrains", "annotations").version("24.1.0")
 
-      plugin("lavalink-gradle-plugin", "com.github.Kamilake.lavalink-gradle-plugin").version("2")
+      plugin("lavalink-gradle-plugin", "com.github.Kamilake").version("2")
 
 
 
